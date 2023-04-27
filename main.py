cbf01db23e6d2c2ae9c75965c9e8990704526dde
Cristian Riano <cristiancarm11@gmail.com>
2023-04-27 12:21:02 +0200

    Add Hello World with openai

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import os
+import openai
 
+from dotenv import load_dotenv
+load_dotenv()
+
+# Load your API key from an environment variable
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def completion_example():
+    return openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print(completion_example())
 
