835a97558142bab6b50a901276464fb4e25f86c4
Konstantin Morozov <konstantin.morozov@glovoapp.com>
2023-04-27 12:42:43 +0200

    chore: hike openai lib version; add fn to retrieve image

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def completion_example():
-    return openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
+    return openai.Completion.create(model="text-davinci-003", prompt="Is Cristian handsome?", temperature=0)
 
+def create_new_image():
+    response = openai.Image.create(prompt="A promotional banner of coca-cola", size="256x256", n=1)
+    return response['data'][0]['url']
 
 if __name__ == '__main__':
-    print(completion_example())
-
+    print(create_new_image())
